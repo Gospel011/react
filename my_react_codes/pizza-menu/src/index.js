5b7875ec9931f15260a4955955bf3b3477efc952
Gospel011 <ugochukwugospeli111@gmail.com>
2024-03-26 04:23:08 +0100

    Adding Components

--- my_react_codes/pizza-menu/src/index.js
+++ my_react_codes/pizza-menu/src/index.js
@@ -1,11 +1,101 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+//* DATA
+const pizzaData = [
+  {
+    name: 'Focaccia',
+    ingredients: 'Bread with italian olive oil and rosemary',
+    price: 6,
+    photoName: 'pizzas/focaccia.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Margherita',
+    ingredients: 'Tomato and mozarella',
+    price: 10,
+    photoName: 'pizzas/margherita.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Spinaci',
+    ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
+    price: 12,
+    photoName: 'pizzas/spinaci.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Funghi',
+    ingredients: 'Tomato, mozarella, mushrooms, and onion',
+    price: 12,
+    photoName: 'pizzas/funghi.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Salamino',
+    ingredients: 'Tomato, mozarella, and pepperoni',
+    price: 15,
+    photoName: 'pizzas/salamino.jpg',
+    soldOut: true,
+  },
+  {
+    name: 'Pizza Prosciutto',
+    ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
+    price: 18,
+    photoName: 'pizzas/prosciutto.jpg',
+    soldOut: false,
+  },
+];
+
 function App() {
-    return <h1>My React App</h1>;
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
 }
 
+function Header() {
+  return <h1>Fast Food Restaurant Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+        <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>We're currently open. {new Date().toLocaleTimeString()}</footer>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <b>Pizza Spinaci</b>
+
+      <p><b>ingredients:</b> Tomato, mozarella, spinach, and ricotta cheese</p>
+
+      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
+    </div>
+  );
+}
+
+// * R E N D E R   R E A C T   A P P
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App/>);
-
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
